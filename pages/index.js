0dca4e07536f65d50003df40ce4c3052a8043e2e
Isaac <ifimbres@uabc.edu.mx>
2022-09-06 22:13:57 -0700

    programatic navigation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home = () => {
+    const router = useRouter();
+
+    const onClick = () => {
+        console.log("Placing your Order");
+        router.push('/products');
+    };
+    
     return <div>
         <Link href="/profile">Go to profile page</Link>
         <Link href="/products">Go to products page</Link>
+        <button type="button" onClick={onClick}>Click here</button>
     </div>
 }
 
