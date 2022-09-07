d7332e8def6d7b8a2ecaaabd85e9514761099bb8
Isaac <ifimbres@uabc.edu.mx>
2022-09-06 22:16:23 -0700

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const PageNotFound = () => {
+    return <div>
+        <div>Page Not Found!</div>
+        <Link href="/">Go to Home Page</Link>
+    </div>
+}
+
+export default PageNotFound;
