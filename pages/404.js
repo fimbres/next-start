6826d6e0e6efa316dcdbfe4380f40d331047c789
Isaac <ifimbres@uabc.edu.mx>
2022-09-15 18:01:58 -0700

    Seo

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,12 @@
 import Link from "next/link";
+import Head from "next/head";
 
 const PageNotFound = () => {
     return <div>
+        <Head>
+            <title>Not Found!</title>
+            <meta name="description" content="Free Stuff" />
+        </Head>
         <div>Page Not Found!</div>
         <Link href="/">Go to Home Page</Link>
     </div>
