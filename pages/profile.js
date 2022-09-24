09c2931bab6dadad77f9c2be406c52719b2bbc46
Isaac <ifimbres@uabc.edu.mx>
2022-09-24 01:46:07 -0700

    styling fixes in the profile page

--- pages/profile.js
+++ pages/profile.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { Box, Paper, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 
 const Profile = () => {
     const { data, status } = useSession();
@@ -15,10 +17,17 @@ const Profile = () => {
     }, [router, status]);
     
     
-    return <div>
-        <h1>{data?.user.email}</h1>
-        <Image src={data?.user.image} alt="Logo" width={200} height={200}/>
-    </div>
+    return <Box sx={{ backgroundColor: "#EEEEEE", height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+        <Container>
+            <Paper elevation={3} sx={{ padding: 15, textAlign: "center" }}>
+                <Image src={data?.user.image} alt="Logo" width={200} height={200} style={{ borderRadius: 15 }}/>
+                <div style={{ marginTop: 15 }}>
+                    <Typography variant="h3" marginBottom={1} marginTop={3}>{data?.user.name}</Typography>
+                    <Typography variant="h6">{data?.user.email}</Typography>
+                </div>
+            </Paper>
+        </Container>
+    </Box>
 }
 
 export default Profile;
